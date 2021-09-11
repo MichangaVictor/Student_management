91e9e41d2794e7ce5db94869e7766671487b6b24
MichangaVictor <onyango.vic@yandex.com>
2021-09-11 17:31:45 +0300

    changes in settings.py

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -124,8 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
@@ -134,6 +135,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
+
+
 
 #For Custom USER
 AUTH_USER_MODEL = "student_management_app.CustomUser"
